e419214e153d8659720d1a23a2e84d9643787ef7
Skoldsjo <skoldsjo.kim@gmail.com>
2021-01-24 18:11:35 +0100

    Add files via upload

---
+++ test/javascript.js
@@ -0,0 +1,18 @@
+
+/*
+const input = document.querySelector('input')
+const form = document.querySelector('form');
+const div = document.createElement('div')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    div.innerText = input.value
+    document.body.append(div)
+}) 
+
+ <form>
+        <input type="text"></input>
+        <button type="submit">Submit</button>
+    </form>
+
+*/
